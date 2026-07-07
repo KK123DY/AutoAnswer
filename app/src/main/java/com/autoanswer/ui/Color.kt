f72c8255cf4a7ef8dc4d48ae427493a975c285dd
KK123DY <385335288@qq.com>
2026-07-07 23:34:20 +0800

    Add app/src/main/java/com/autoanswer/ui/Color.kt

---
+++ app/src/main/java/com/autoanswer/ui/Color.kt
@@ -0,0 +1,24 @@
+package com.autoanswer.ui
+
+import androidx.compose.ui.graphics.Color
+
+// 主色
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// 自定义颜色
+val Primary = Color(0xFF6200EE)
+val PrimaryVariant = Color(0xFF3700B3)
+val Secondary = Color(0xFF03DAC5)
+val Background = Color(0xFFF5F5F5)
+val Surface = Color(0xFFFFFFFF)
+val Error = Color(0xFFB00020)
+val OnPrimary = Color(0xFFFFFFFF)
+val OnSecondary = Color(0xFF000000)
+val Success = Color(0xFF4CAF50)
+val Warning = Color(0xFFFF9800)
